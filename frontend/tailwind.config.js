b0bcf1de1655e2ba13dc0def97b649be7ee68f0d
ethanglide <ecook2489@gmail.com>
2024-02-26 20:54:23 -0500

    Add home page start

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['SF-Compact-Rounded-Regular']
+    },
+    fontSize: {
+      sm: '1rem',
+      base: '1.25rem',
+      lg: '1.55rem',
+      xl: '1.8rem',
+      '2xl': '2rem',
+      '3xl': '2.5rem',
+      '4xl': '3rem',
+      '5xl': '4rem',
+    },
     extend: {},
   },
   plugins: [],
