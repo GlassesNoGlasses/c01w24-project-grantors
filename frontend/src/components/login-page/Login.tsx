d7f9bd108cee06eacf425a10fb6943153a0da0ce
francisayyad03 <francis.ayyad@mail.utoronto.ca>
2024-02-27 00:17:33 -0500

    first prototype of loginForm

--- frontend/src/components/login-page/Login.tsx
+++ frontend/src/components/login-page/Login.tsx
@@ -1,12 +1,57 @@
-import React from 'react'
-import { LoginProps } from './LoginProps'
+import React, { useState } from 'react';
+import { LoginProps } from './LoginProps';
+import './Login.css';
+import emailIcon from '../../images/iconMail.png';
+import passwordIcon from '../../images/iconPassword.png';
+import passwordHidden from '../../images/eyeClose.png.png';
+import passwordShow from '../../images/eyeOpen.png.png';
 
-const Login = ({
-  // Add props here
-}: LoginProps) => {
+const Login: React.FC<LoginProps> = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-container">
+      <div className="login-box">
+        <h3 className="title">Log in to your account</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <img src={emailIcon} alt="Email" className="input-icon" />
+            <input
+              type="text"
+              id="email"
+              placeholder="Enter email or username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-input"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <img src={passwordIcon} alt="Password" className="input-icon" />
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-input"
+              required
+            />
+          </div>
+          <div className="flex items-baseline justify-between">
+            <button type="submit" className="submit-btn">Login</button>
+            <a href="#" className="forgot-password">Forgot password?</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'logoGreen': '#70C03E',
+      },
+    },
   },
   plugins: [],
 }
