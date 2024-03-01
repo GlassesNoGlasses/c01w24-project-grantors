821ae284519a627be0e4785520641bcd13daa5a1
Alex Zeng <alex.zenggg@gmail.com>
2024-02-29 19:54:58 -0500

    backend for login finished but not tested

--- backend/server.mjs
+++ backend/server.mjs
@@ -4,7 +4,7 @@ import { MongoClient, ObjectId } from "mongodb";
 const app = express();
 const PORT = 8000;
 const MONGO_URL = "mongodb://localhost:27017";
-const DB_NAME = "test";
+const DB_NAME = "grantors";
 
 // Connect to MongoDB
 let db;
@@ -32,4 +32,41 @@ app.listen(PORT, () => {
 // Routes
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
+});
+
+app.post('/login', express.json(), async (req, res) => {
+  try {
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ error: "Username and password both needed to login." });
+    }
+
+    const collection = db.collection("users");
+    const uname = await collection.findOne({ username:username});
+    const email = await collection.findOne({ email:username});
+
+    if (!uname && !email) {
+      return res.status(404).json({'error' : 'User Not Found'});
+    }
+
+    if (!uname) {
+      if (!(email.password === password)) {
+        return res.status(401).json({'error' : 'Incorrect Credentials'});
+      }
+      return res.status(200).send({ admin:email.isAdmin});
+    } else if (!(uname.password === password)){
+      return res.status(404).json({'error' : 'Incorrect Credentials'});
+    }
+
+    return res.status(200).send({ admin:uname.isAdmin});
+
+  } catch {
+    res.status(500).send('Server Error with Logging In');
+  }
+  
+  
 });
