4c4671ee838dc9375f7f5e44cb8bf4ffc548d002
ZackSteine <zack.steine@mail.utoronto.ca>
2024-03-03 15:16:52 -0500

    Added logout link

--- frontend/src/components/home-page/nav-bar/client-dashboard/ClientDashboard.tsx
+++ frontend/src/components/home-page/nav-bar/client-dashboard/ClientDashboard.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useUserContext } from '../../../contexts/userContext';
 import ButtonIcon from '../../../displays/ButtonIcon/ButtonIcon';
-import { Cog6ToothIcon, ArrowRightStartOnRectangleIcon, UserIcon, StarIcon, ListBulletIcon, DocumentMagnifyingGlassIcon} from '@heroicons/react/24/solid';
+import { Cog6ToothIcon, ArrowRightStartOnRectangleIcon, UserIcon, StarIcon, ListBulletIcon} from '@heroicons/react/24/solid';
 import search from '../../../../images/search.png'
 import ApplicationIcon from '../../../displays/ApplicationIcon/ApplicationIcon';
+import { Link } from 'react-router-dom';
 
 
 const ClientDashboard = () => {
@@ -13,14 +14,16 @@ const ClientDashboard = () => {
     <div className="dashboard-container bg-lime-500 w-full h-full">
       <div className="welcome-message underline px-10 bold text-4xl">Welcome{user ? ", " + user.username + "!" : ""}</div>
       <div className="client-butons flex justify-around items-center p-10">
-        <ButtonIcon heroicon={<StarIcon className=""/>} label="Saved Grants"/>
-        <ButtonIcon heroicon={<UserIcon className=""/>} label="Account"/>
+        <ButtonIcon heroicon={<StarIcon />} label="Saved Grants"/>
+        <ButtonIcon heroicon={<UserIcon />} label="Account"/>
         <ButtonIcon heroicon={<Cog6ToothIcon className=""/>} label="Settings"/>
-        <ButtonIcon heroicon={<ArrowRightStartOnRectangleIcon className=""/>} label="Log out"/>
+        <Link to="/">
+          <ButtonIcon heroicon={<ArrowRightStartOnRectangleIcon />} label="Log out" callback={() => setUser(null)}/>
+        </Link>
       </div>
       <div className="application-buttons flex justify-around items-center">
-        <ApplicationIcon heroicon={<ListBulletIcon/>} label="View Available Grants" />
-        <ApplicationIcon imageSrc={search} label="My Applications"/>
+        <ApplicationIcon heroicon={<ListBulletIcon className="h-40 w-40"/>} label="View Available Grants" />
+        <ApplicationIcon imageSrc={search} label="My Applications" />
       </div>
     </div>
   )
