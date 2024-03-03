813d711f769f153be81f5dce3a29c8b082934bc8
ethanglide <ecook2489@gmail.com>
2024-03-03 14:07:50 -0500

    Change SignUp to use tailwind

--- frontend/src/components/signup-page/SignUp.tsx
+++ frontend/src/components/signup-page/SignUp.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { SignUpProps } from './SignUpProps';
-import './SignUp.css';
 import nameIcon from '../../images/iconName.png';
 import emailIcon from '../../images/iconMail.png';
 import passwordIcon from '../../images/iconPassword.png';
@@ -34,64 +33,75 @@ const SignUp: React.FC<SignUpProps> = () => {
   };
 
   return (
-    <div className="signup-container">
-      <div className="signup-box">
-        <h3 className="title">Sign up to Grantors</h3>
-        <form onSubmit={handleSubmit}>
-          <div className="input-group">
-            <img src={nameIcon} alt="Password" className="input-icon" />
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div 
+        className="w-full max-w-xs px-4 py-6 mx-auto bg-white shadow rounded-lg 
+          sm:px-6 sm:py-8 md:max-w-sm lg:max-w-md xl:max-w-lg 2xl:max-w-xl
+          flex flex-col gap-4"
+        style={{ boxShadow: '-10px 10px 30px 0 rgba(0, 0, 0, 0.1)' }}>
+        <h3 className="text-xl font-bold text-center sm:text-2xl text-gray-700">Sign up to Grantors</h3>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+          <div className="flex items-center">
+            <img src={nameIcon} alt="Password"
+              className="mr-2 h-5 w-5 flex-shrink-0 text-green-500" />
             <input
               type="firstName"
               id="firstName"
               placeholder="Enter your First Name"
               value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
-              className="form-input"
+              className="w-full px-3 py-2 mt-2 border border-gray-200 rounded-full focus:outline-none
+                focus:ring focus:ring-green-600 sm:px-4 sm:py-3"
+              style={{ boxShadow: 'inset -4px 4px 6px rgba(0, 0, 0, 0.1)'}}
               required
             />
           </div>
-          <div className="input-group">
-            <img src={nameIcon} alt="Password" className="input-icon" />
+          <div className="flex items-center">
+            <img src={nameIcon} alt="Password"
+              className="mr-2 h-5 w-5 flex-shrink-0 text-green-500" />
             <input
               type="lastName"
               id="lastName"
               placeholder="Enter your Last Name"
               value={lastName}
               onChange={(e) => setLastName(e.target.value)}
-              className="form-input"
+              className="w-full px-3 py-2 mt-2 border border-gray-200 rounded-full focus:outline-none
+                focus:ring focus:ring-green-600 sm:px-4 sm:py-3"
+              style={{ boxShadow: 'inset -4px 4px 6px rgba(0, 0, 0, 0.1)'}}
               required
             />
           </div>
-          <div className="input-group">
-            <img src={emailIcon} alt="Email" className="input-icon" />
+          <div className="flex items-center">
+            <img src={emailIcon} alt="Email"
+              className="mr-2 h-5 w-5 flex-shrink-0 text-green-500" />
             <input
               type="text"
               id="email"
               placeholder="Enter Email or Username"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="form-input"
+              className="w-full px-3 py-2 mt-2 border border-gray-200 rounded-full focus:outline-none
+                focus:ring focus:ring-green-600 sm:px-4 sm:py-3"
+              style={{ boxShadow: 'inset -4px 4px 6px rgba(0, 0, 0, 0.1)'}}
               required
             />
           </div>
-          <div className="input-group">
-            <img src={passwordIcon} alt="Password" className="input-icon" />
+          <div className="flex items-center">
+            <img src={passwordIcon} alt="Password"
+              className="mr-2 h-5 w-5 flex-shrink-0 text-green-500" />
             <input
               type="password"
               id="password"
               placeholder="Enter Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="form-input"
+              className="w-full px-3 py-2 mt-2 border border-gray-200 rounded-full focus:outline-none
+                focus:ring focus:ring-green-600 sm:px-4 sm:py-3"
+              style={{ boxShadow: 'inset -4px 4px 6px rgba(0, 0, 0, 0.1)'}}
               required
             />
           </div>
-          <div className="flex justify-center">
-            <button type="submit" className="submit-btn">Sign Up</button>
-          </div>
-        </form>
-        <div className="is-admin-button">
-          <label>
+          <label className='flex flex-row gap-2 text-blue-700 text-sm'>
             Sign up as Admin?
             <input
               type="checkbox"
@@ -99,7 +109,14 @@ const SignUp: React.FC<SignUpProps> = () => {
               onChange={() => setIsAdmin(!isAdmin)}
             />
           </label>
-        </div>
+          <div className="flex justify-center">
+            <button
+              type="submit" 
+              className='p-2 px-5 m-2 bg-blue-500 hover:bg-blue-600 active:bg-blue-700
+              text-white font-bold rounded-lg shadow-md transition-colors duration-150 ease-in
+                text-base'>Sign Up</button>
+          </div>
+        </form>
       </div>
     </div>
   );
