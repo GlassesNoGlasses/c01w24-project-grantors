4d0316d8646f573ba6606dba9c79892e02020e39
SheeperGit <126368846+SheeperGit@users.noreply.github.com>
2024-03-03 11:27:10 -0500

    added isAdmin button, firstName field, lastName field

--- frontend/src/components/signup-page/SignUp.tsx
+++ frontend/src/components/signup-page/SignUp.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { SignUpProps } from './SignUpProps';
 import './SignUp.css';
+import nameIcon from '../../images/iconName.png';
 import emailIcon from '../../images/iconMail.png';
 import passwordIcon from '../../images/iconPassword.png';
 
 const SignUp: React.FC<SignUpProps> = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -35,6 +39,30 @@ const SignUp: React.FC<SignUpProps> = () => {
         <h3 className="title">Sign up to Grantors</h3>
         <form onSubmit={handleSubmit}>
           <div className="input-group">
+            <img src={nameIcon} alt="Password" className="input-icon" />
+            <input
+              type="firstName"
+              id="firstName"
+              placeholder="Enter your First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              className="form-input"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <img src={nameIcon} alt="Password" className="input-icon" />
+            <input
+              type="lastName"
+              id="lastName"
+              placeholder="Enter your Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className="form-input"
+              required
+            />
+          </div>
+          <div className="input-group">
             <img src={emailIcon} alt="Email" className="input-icon" />
             <input
               type="text"
@@ -62,6 +90,16 @@ const SignUp: React.FC<SignUpProps> = () => {
             <button type="submit" className="submit-btn">Sign Up</button>
           </div>
         </form>
+        <div className="is-admin-button">
+          <label>
+            Sign up as Admin?
+            <input
+              type="checkbox"
+              checked={isAdmin}
+              onChange={() => setIsAdmin(!isAdmin)}
+            />
+          </label>
+        </div>
       </div>
     </div>
   );
