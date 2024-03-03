98209eb10a7ed2969dcd938e724db0c24a7d265e
Juno <nightstraps@gmail.com>
2024-03-03 12:25:32 -0500

    working /registerUsers POST call, jest test suite not running

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,10 +1,16 @@
 import express from 'express';
 import { MongoClient, ObjectId } from "mongodb";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const PORT = 8000;
-const MONGO_URL = "mongodb://localhost:27017";
-const DB_NAME = "test";
+const MONGO_URL = "mongodb://127.0.0.1:27017";
+const DB_NAME = "data";
+const COLLECTIONS = {
+  users: "users",
+};
+
 
 // Connect to MongoDB
 let db;
@@ -32,4 +38,43 @@ app.listen(PORT, () => {
 // Routes
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
+});
+
+app.post("/registerUser", express.json(), async (req, res) => {
+  try {
+    const {accountID, username, email, password, firstName, lastName, isAdmin} = req.body;
+
+    // Basic body request check
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ error: "Username and password both needed to register." });
+    }
+
+    // Checking if username does not already exist in database
+    const userCollection = db.collection(COLLECTIONS.users);
+    const existingUser = await userCollection.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username already exists." });
+    }
+
+    // Creating hashed password 
+    // and storing user info in database
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await userCollection.insertOne({
+      accountID: accountID,
+      username,
+      email: email,
+      password: hashedPassword,
+      firstName: firstName,
+      lastName: lastName,
+      isAdmin: isAdmin
+    });
+
+    // Returning JSON Web Token
+    const token = jwt.sign({ username }, "secret-key", { expiresIn: "1h" });
+    res.status(201).json({ response: "User registered successfully.", token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
