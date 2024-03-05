57d88d1820243f4f28ed872c9f304f2a477b04fe
ethanglide <ecook2489@gmail.com>
2024-03-04 20:27:22 -0500

    Add GrantList component

---
+++ frontend/src/components/interfaces/Grant.ts
@@ -0,0 +1,11 @@
+export interface Grant {
+    id: Number,
+    title: String,
+    description: String,
+    posted: Date,
+    deadline: Date,
+    amount: Number,
+    organization: String,
+    category: String,
+    contact: String,
+}
