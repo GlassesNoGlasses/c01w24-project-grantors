ce675f4c8a98b82f75d467daf32ec3fd5c889091
Neil Wang <neilywang3@gmail.com>
2024-03-11 21:23:33 -0400

    Adde Modal component and Signup functionalties

---
+++ frontend/src/components/modal/ModalWrapper.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ModalWrapperProps } from './ModalWrapperProps'
+import ModalInner from './ModalInner'
+
+const ModalWrapper = ({
+        children,
+        closeModal,
+}: ModalWrapperProps) => {
+
+    return (
+        <div onClick={closeModal} className='min-h-full min-w-full
+        flex justify-center items-center z-10 fixed inset-0 bg-dim '>
+                <ModalInner innerClick={(e: React.MouseEvent) =>  e.stopPropagation()}>
+                        {children}
+                </ModalInner>
+        </div>
+    )
+}
+
+export default ModalWrapper
