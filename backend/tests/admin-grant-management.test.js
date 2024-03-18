a909a54d685c00a3afa2438c114d720995731d0c
ZackSteine <zack.steine@mail.utoronto.ca>
2024-03-18 19:40:13 -0400

    Cleaned up test

--- backend/tests/admin-grant-management.test.js
+++ backend/tests/admin-grant-management.test.js
@@ -5,25 +5,6 @@ let gId1 = ''
 let gId2 = ''
 
 test("POST /grant - 201 Grant Saved (Not Published) to MongoDB", async() => {
-
-    const signup = await fetch(`${SERVER_URL}/signup`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          isAdmin: false,
-          username: "dude",
-          firstName: "neil",
-          lastName: "broski",
-          email: "neily@mail",
-          password: "123"
-    })});
-
-    const lol = signup.json().then((data) => {
-        accId = data['id']
-    })
-
     const res = await fetch(`${SERVER_URL}/grant`, {
         method: 'POST',
         headers: {
@@ -32,13 +13,13 @@ test("POST /grant - 201 Grant Saved (Not Published) to MongoDB", async() => {
         body: JSON.stringify({ 'title': 'test', 'description': 'test grant', 
         'deadline': "2024-03-13T00:16:33.451Z", 'minAmount': 0, "maxAmount": 1, 'organization': 'us',
         'category': 'one', "contact": 'my number', 'questions': [], 'publish': false }),
-    })
+    });
 
-    const lmao = res.json().then((data) => {
+    await res.json().then((data) => {
         gId1 = data['id']
-    })
+    });
 
-    expect(res.status).toBe(201)
+    expect(res.status).toBe(201);
 })
 
 test("POST /grant - 201 Grant Saved (Published) to MongoDB", async() => {
@@ -51,17 +32,14 @@ test("POST /grant - 201 Grant Saved (Published) to MongoDB", async() => {
         body: JSON.stringify({ 'title': 'publish', 'description': 'publish grant', 
         'deadline': "2024-03-13T00:16:33.451Z", 'minAmount': 0, "maxAmount": 1, 'organization': 'us',
         'category': 'one', "contact": 'my number', 'questions': [], 'publish': false }),
-    })
+    });
 
-    res.json().then((data) => {
+    await res.json().then((data) => {
         gId2 = data['id']
-    })
+    });
 
-    expect(res.status).toBe(201)
-})
-
-
-
+    expect(res.status).toBe(201);
+});
 
 test("GET /grant - 404 grant not found", async() => {
 
@@ -70,16 +48,13 @@ test("GET /grant - 404 grant not found", async() => {
         headers: {
         'Content-Type': 'application/json',
         },
-    })
+    });
 
-    const body = await res.json()
+    const body = await res.json();
 
     expect(res.status).toBe(404)
     expect(body['error']).toBe("Unable to find grant with given ID.")
-})
-
-
-
+});
 
 test("GET /grant - 200 published grant found", async() => {
 
@@ -88,14 +63,10 @@ test("GET /grant - 200 published grant found", async() => {
         headers: {
         'Content-Type': 'application/json',
         },
-    })
+    });
 
-    const body = await res.json()
-
-    expect(res.status).toBe(200)
-})
-
-
+    expect(res.status).toBe(200);
+});
 
 test("PUT /grant - 201 unpublished grant edited", async() => {
 
@@ -104,10 +75,10 @@ test("PUT /grant - 201 unpublished grant edited", async() => {
         headers: {
         'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 'accId': accId, 'title': 'test edited', 'description': 'test grant', 
+        body: JSON.stringify({ 'title': 'test edited', 'description': 'test grant', 
         'deadline': "2024-03-13T00:16:33.451Z", 'minAmount': 0, "maxAmount": 1, 'organization': 'us',
         'category': 'one', "contact": 'my number', 'questions': [], 'publish': false }),
-    })
+    });
 
     const resp = await fetch(`${SERVER_URL}/grant/${gId1}`, {
         method: 'GET',
@@ -120,7 +91,7 @@ test("PUT /grant - 201 unpublished grant edited", async() => {
 
     expect(res.status).toBe(201);
     expect(body['response'].title).toBe('test edited');
-})
+});
 
 test("DELETE /grant - 201 unpublished grant deleted", async() => {
 
@@ -129,15 +100,15 @@ test("DELETE /grant - 201 unpublished grant deleted", async() => {
         headers: {
         'Content-Type': 'application/json',
         },
-    })
+    });
 
     const resp = await fetch(`${SERVER_URL}/grant/${gId1}`, {
         method: 'GET',
         headers: {
         'Content-Type': 'application/json',
         },
-    })
+    });
 
-    expect(res.status).toBe(200)
-    expect(resp.status).toBe(404)
-})
+    expect(res.status).toBe(200);
+    expect(resp.status).toBe(404);
+});
