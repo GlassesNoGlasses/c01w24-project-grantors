bd140426f7ebe0b9668778e7f7e867fa82c5bd56
ZackSteine <zack.steine@mail.utoronto.ca>
2024-03-21 00:06:37 -0400

    Show applicant name in AdminApplicantList instead of id

--- frontend/src/components/home-page/nav-bar/admin-dashboard/AdminApplicationList.tsx
+++ frontend/src/components/home-page/nav-bar/admin-dashboard/AdminApplicationList.tsx
@@ -6,40 +6,52 @@ import { useParams, useNavigate } from 'react-router-dom';
 import Table from '../../../table/Table';
 import { Column } from '../../../table/TableProps';
 import ApplicationsController from '../../../../controllers/ApplicationsController';
+import UserController from '../../../../controllers/UserController';
+import { Applicant } from '../../../../interfaces/Applicant';
+
+type TableData = [Application, Applicant];
 
 const AdminApplicationList = ({}: AdminApplicationListProps) => {
     const { user, setUser } = useUserContext();
     const [ applications, setApplications ] = useState<Application[]>([]);
+    const [ applicants, setApplicants ] = useState<Applicant[]>([]);
+    const [ tableData, setTableData ] = useState<TableData[]>([]);
     const { organization } = useParams();
     const navigate = useNavigate();
 
+    const applicantNotFound: Applicant = { id: '',
+        firstName: 'Applicant',
+        lastName: 'not found.',
+        email: '',
+    }
+
     const itemsPerPageOptions: number[] = [5,10,20,50,100];
-    const columns: Column<Application>[] = [
+    const columns: Column<TableData>[] = [
         {
             title: "Grant Titlte",
-            format: (application: Application) => application.grantTitle,
-            sort: (app1: Application, app2: Application) => app1.grantTitle < app2.grantTitle ? -1 : 1,
+            format: (data: TableData) => data[0].grantTitle,
+            sort: (data1: TableData, data2: TableData) => data1[0].grantTitle < data2[0].grantTitle ? -1 : 1,
         },
         {
             title: "Applicant",
-            format: (application: Application) => String(application.applicantID),
-            sort: (app1: Application, app2: Application) => app1.applicantID < app2.applicantID ? -1 : 1,
+            format: (data: TableData) => data[1].firstName + ' ' + data[1].lastName,
+            sort: (data1: TableData, data2: TableData) => data1[1].firstName + data1[1].lastName < data2[1].firstName + data2[1].lastName ? -1 : 1,
         },
         {
             title: "Date",
-            format: (application: Application) => {
-                return application.submissionDate.toLocaleDateString('en-GB', {
+            format: (data: TableData) => {
+                return data[0].submissionDate.toLocaleDateString('en-GB', {
                     day: '2-digit',
                     month: 'short',
                     year: 'numeric'
                 })
             },
-            sort: (app1: Application, app2: Application) => Number(app1.submissionDate) - Number(app2.submissionDate),
+            sort: (data1: TableData, data2: TableData) => Number(data1[0].submissionDate) - Number(data2[0].submissionDate),
         },
     ];
 
-    const onApplicationRowClick = (application: Application) => {
-        navigate(`/application/${application.id}/review`);
+    const onApplicationRowClick = (data: TableData) => {
+        navigate(`/application/${data[0].id}/review`);
     }
 
     useEffect(() => {
@@ -63,10 +75,28 @@ const AdminApplicationList = ({}: AdminApplicationListProps) => {
 
     }, [user, navigate]);
 
+    useEffect(() => {
+        UserController.fetchApplicants(applications.map((application: Application) => {
+            return application.applicantID; 
+        })).then((applicants: Applicant[]) => {
+            setApplicants(applicants);
+        });
+
+    }, [applications]);
+
+    useEffect(() => {
+        if (applicants) {
+            setTableData(applications.filter((app) => app.submitted).map((app: Application) => {
+                return [app, applicants.find((applicant: Applicant) => applicant.id === app.applicantID) ?? applicantNotFound];
+            }));
+        }
+
+    }, [applications, applicants]);
+
     return (
         <div className="flex flex-col h-full items-start justify-start px-5 bg-grantor-green">
             <span className="text-2xl pl-2">{organization} Grant Applications</span>
-            <Table<Application> items={applications.filter((app) => app.submitted)}
+            <Table<TableData> items={tableData}
                    columns={columns}
                    itemsPerPageOptions={itemsPerPageOptions}
                    defaultIPP={10}
