a0309c578747998a04bc3e7146da49c37e400d9c
Alex Zeng <alex.zenggg@gmail.com>
2024-03-22 02:44:26 -0400

    refactored navbar for admin and user, refactored page for admin dashboard

--- frontend/src/components/displays/ApplicationIcon/ApplicationIcon.tsx
+++ frontend/src/components/displays/ApplicationIcon/ApplicationIcon.tsx
@@ -6,7 +6,9 @@ const ApplicationIcon = ({
     label
 }: ApplicationIconProps) => {
     return (
-        <button className='flex flex-col items-center bg-white rounded-lg p-4 px-6 gap-2'>
+        <button className='flex flex-col items-center bg-white rounded-lg gap-2
+        border-4 border-primary shadow-2xl hover:border-black shadow-black min-w-96
+        min-h-72 justify-center'>
             <div className='max-h-48 max-w-48'>
                 {imageSrc ? (
                     <img src={imageSrc}></img>
