1dab781ce7ee645a250fda827fe6f7c282a4d878
ZackSteine <zack.steine@mail.utoronto.ca>
2024-03-25 13:49:23 -0400

    Removed unused variables

--- frontend/src/components/filter/DropDownFilter.tsx
+++ frontend/src/components/filter/DropDownFilter.tsx
@@ -1,18 +1,9 @@
 import { useEffect, useState } from "react";
 import { DropDownFilterProps } from "./DropDownFilterProps";
-import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import DropDown from "../displays/DropDown/DropDown";
 
 function DropDownFilter({label, options, identity, setFilter, className}: DropDownFilterProps ) {
-    const [isOpen, setIsOpen] = useState(false);
     const [selectedItem, setSelectedItem] = useState(identity);
-
-    const handleSelect = (option: string) => {
-        setSelectedItem(option);
-        setIsOpen(false);
-    };
-
-    const dropDownList = [identity, ...options];
 
     useEffect(() => {
         setFilter(selectedItem);
