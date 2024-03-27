a694b3c89ef8379e9592475399fb701ef62fe60f
ethanglide <ecook2489@gmail.com>
2024-03-27 10:30:31 -0400

    Add MilestoneItem

--- frontend/src/components/milestones-page/UserMilestones.tsx
+++ frontend/src/components/milestones-page/UserMilestones.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import ApplicationsController from "../../controllers/ApplicationsController";
 import { Application, ApplicationStatus } from "../../interfaces/Application";
 import { useUserContext } from "../contexts/userContext";
+import { GrantMilstone } from "../../interfaces/Grant";
+import { Check, X } from "heroicons-react";
 
 const UserMilestonesPage = () => {
     const { user } = useUserContext();
@@ -40,5 +42,30 @@ const UserMilestonesPage = () => {
         </div>
     );
 };
+
+const MilestoneItem = ({ milestone }: { milestone: GrantMilstone }) => {
+    return (
+        <div className="flex flex-col gap-2 p-2 px-5">
+            <div className="flex flex-row justify-between align-middle">
+                <h3 className="text-lg font-bold">{milestone.title}</h3>
+                {milestone.completed ? <Check className="h-5 w-5 text-green-500" /> : <X className="h-5 w-5 text-red-500" />}
+            </div>
+            <p className="text-sm">Due: {new Date(milestone.dueDate).toDateString()}</p>
+            <p className="text-base">{milestone.description}</p>
+            <form className="flex flex-col gap-2">
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="evidence">Evidence</label>
+                    <textarea name="evidence" id="evidence" required></textarea>
+                </div>
+                <button type="submit" 
+                    className='p-2 px-5 m-2 bg-primary hover:bg-secondary 
+                    text-white font-bold rounded-lg shadow-md transition-colors duration-150 ease-in
+                    text-base'>
+                    Submit Milestone
+                </button>
+            </form>
+        </div>
+    )
+}
 
 export default UserMilestonesPage;
